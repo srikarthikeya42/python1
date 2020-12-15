c3d99d75220d8527281c3ecd820bb0e3f7fa05e0
srikarthikeya42 <70191374+srikarthikeya42@users.noreply.github.com>
2020-12-15 10:38:37 +0530

    Create Linkedlist_useript.py

---
+++ Linkedlist_useript.py
@@ -0,0 +1,30 @@
+Sri karthikeya
+121910313042"""
+#Linked list with user input
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+    def append(self,data):
+        if self.tail is None:
+            self.head=Node(data)
+            self.tail=self.head
+        else:
+            self.tail.next=Node(data)
+            self.tail=self.tail.next
+    def disp(self):
+        cur=self.head 
+        while cur is not None:
+            print(cur.data,end=" ")
+            cur=cur.next
+l=LinkedList()
+n=int(input("size:"))
+for i in range(n):
+    data=int(input())
+    l.append(data)
+l.disp()            
